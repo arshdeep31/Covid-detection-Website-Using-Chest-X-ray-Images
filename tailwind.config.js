d610a9fa86441b47d2dafd2ba2fbfd89efd185ec
Arshdeep Singh <45181080+arshdeep31@users.noreply.github.com>
2022-01-23 13:16:34 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const colors = require("tailwindcss/colors");
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {},
+    colors: {
+      green: colors.green,
+      gray: colors.gray,
+      red: colors.red,
+      blue: colors.blue,
+      black: colors.black,
+      white: colors.white,
+    },
+    fontFamily: {
+      body: ["Poppins", "sans-serif"],
+    },
+  },
+  variants: {
+    extend: {},
+    animation: ["responsive", "motion-safe", "motion-reduce"],
+  },
+  plugins: [],
+};
